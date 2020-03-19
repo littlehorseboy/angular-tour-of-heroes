6dc38a9477b49aa1c89764cc8978e97e9c67acba
littlehorseboy <littlehorseboy@gmail.com>
2020-03-19 14:46:25 +0800

    feat: hero.service Observable<Hero[]> subscribe message.component，message.service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { MessageService } from './message.service';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(
+    public messageService: MessageService,
+  ) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
