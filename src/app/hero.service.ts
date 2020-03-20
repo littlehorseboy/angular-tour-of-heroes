88465421d26b67d59651ede613367ddc36dbdb99
littlehorseboy <littlehorseboy@gmail.com>
2020-03-20 16:51:44 +0800

    feat: 將 heroes 來源改成由 HttpClient 撈取
    刪除 mock-heroes.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,52 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { MessageService } from './message.service';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';
 
   constructor(
-    public messageService: MessageService,
+    private http: HttpClient,
+    private messageService: MessageService,
   ) { }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(() => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', [])),
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: getched hero id = ${id}`);
-    return of(HEROES.find((hero) => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap(() => this.log(`fetched hero id = ${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=  ${id}`)),
+      );
+  }
+
+  log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
   }
 }
